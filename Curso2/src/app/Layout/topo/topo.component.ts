3dae9f979a41837da0005d1620f2f7e3ec0c703b
adrileyapp <adriley@terra.com.br>
2017-10-23 02:35:52 -0200

    ultima versão
    ultima versão

---
+++ Curso2/src/app/Layout/topo/topo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topo',
+  templateUrl: './topo.component.html'
+})
+export class TopoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Curso2/src/app/app.module.ts
+++ Curso2/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopoComponent } from './Layout/topo/topo.component';
+import { ProgressoComponent } from './progresso/progresso.component';
+import { TentativaComponent } from './tentativa/tentativa.component';
+import { PainelComponent } from './painel/painel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    ProgressoComponent,
+    TentativaComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule
