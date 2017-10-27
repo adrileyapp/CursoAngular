a88b376c34fb0eb34a15e8039606294aaf373cf6
adrileyapp <adriley@terra.com.br>
2017-10-27 14:10:29 -0200

    FASE 07
    FASE 07

--- Curso2/src/app/painel/painel.component.ts
+++ Curso2/src/app/painel/painel.component.ts
@@ -14,13 +14,21 @@ export class PainelComponent implements OnInit {
 public frases: any;
 public instrucao : string = 'Traduza a frase ';
 public Resposta : string;
+public Rodada:number =0;
+public RaodaFase: Frase;
   constructor(private servicosFraseService: ServicosFraseService) {
     this.frases = this.servicosFraseService.GetFrases();
     console.log(this.frases);
    }
     atualizaResposta(Resposta : Event):void{
       this.Resposta = (<HTMLInputElement>Resposta.target).value;
-      console.log(this.Resposta);
+      //console.log(this.Resposta);
+    }
+    verificarResposta():void{
+   //   console.log('Verificar Resposta : '+ this.Resposta );
+      alert(this.Resposta);
+
+
     }
   ngOnInit() {
   }
